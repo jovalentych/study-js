b4303c8e2f6ff6e187fc9c1d1e88afd93c00b140
Yovan H.-V <jovalentych@gmail.com>
2023-02-02 20:51:50 +0200

    First commit

---
+++ js/app001.js
@@ -0,0 +1,54 @@
+// document.write("Hello world with document.write()");
+// console.log("Hello world with cinsole.log()");
+// console.info("Hello world with cinsole.info()");
+// console.error("Error info with console.error()");
+// console.warn("Warn with warn()");
+
+// let number = 25;
+// console.log(number);
+// number = 666;
+// console.log(`Variable number = ${number}`);
+
+// let number = 251;
+// if (number == 25) {
+//   console.log("YASS");
+// } else {
+//   console.log("NOPE");
+// }
+
+// let num = 100;
+
+// for (let i = 0; i <= num; i += 5) {
+//   console.log(i);
+// }
+
+// const array = [1, 2, 3, 4, 5, 6, 7, 342, 234, 324575, 6435, 3432];
+
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// // }
+
+// let num = 100;
+// for (let i = 0; i <= num; i += 5) {
+//   console.log(i);
+// }
+
+// alert("hello world");
+
+// let data = confirm("Do you like it?");
+
+// document.write(data);
+
+// prompt("How old are you?");
+
+// let person = null;
+// if (confirm("Are you sure?")) {
+//   person = prompt("Enter your name!");
+//   alert(`Hello, ${person}`);
+// } else {
+//   alert("You have chosen CANCEL");
+// }
+
+// function onClickButton() {
+//   alert("You have pushed me!!!");
+// }
