e95b3a34f622abd051cf0df693ba163582e779f0
Yovan H.-V <jovalentych@gmail.com>
2023-02-03 09:43:49 +0200

    Add code with movie programm with loops

--- js/app002.js
+++ js/app002.js
@@ -20,7 +20,7 @@
     }
 
 Проверить, чтобы все работало без ошибок в консоли */
-
+/*
 "use strict";
 
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
@@ -40,5 +40,60 @@ let a = prompt("Один из последних просмотренных фи
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+*/
+
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+"use strict";
+
+// Код возьмите из предыдущего домашнего задания
+
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+let counter = numberOfFilms;
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+while (counter != 0) {
+  let a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = +prompt("На сколько оцените его?", "");
+
+  if (a != null && b != null && a != "" && b != "" && a.length <= 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("DONE");
+  } else {
+    console.log("ERROR");
+    counter++;
+  }
+  counter--;
+}
+
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+  alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+  alert("Вы киноман");
+} else {
+  alert("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
---
+++ js/testJS-sheet.js
@@ -0,0 +1,44 @@
+function fourthTask() {
+  // Пишем решение вот тут
+  let i = 16;
+  while (i != 16) {
+    if (i % 2 != 0) {
+      console.log(i);
+    }
+  }
+}
+
+fourthTask();
+
+function fifthTask() {
+  const arrayOfNumbers = [];
+  let val;
+  // Пишем решение вот тут
+  for (let i = 5; i <= 10; i++) {
+    arrayOfNumbers.push[val];
+    val = i;
+  }
+  console.log(arrayOfNumbers.length);
+  // Не трогаем
+  return arrayOfNumbers;
+}
+console.log(fifthTask(arrayOfNumbers.length));
+
+// Место для второй задачи
+function secondTask() {
+  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+  const data = [5, 10, "Shopping", 20, "Homework"];
+
+  // Пишем решение вот тут
+  for (let i = 0; i < data.length; i++) {
+    if (typeof data[i] == Number) {
+      data[i] *= 2;
+    } else {
+      `${data[i]} - done`;
+    }
+  }
+
+  // Не трогаем
+  return data;
+}
+secondTask(console.log(data));
