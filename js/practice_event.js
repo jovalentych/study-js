810f71ebdb5377733c33ded62494d6e2014ce006
Yovan H.-V <jovalentych@gmail.com>
2023-02-07 21:28:45 +0200

    Add updates

---
+++ js/practice_event.js
@@ -0,0 +1,7 @@
+("use strict");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector(".btn-primary");
+
+  console.log(button.addEventListener("click"));
+});
